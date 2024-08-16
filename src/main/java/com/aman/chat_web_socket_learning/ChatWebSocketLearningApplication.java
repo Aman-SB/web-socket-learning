9d4d469f7e42a6582d4edc07f0a7e002a4ff4dd9
Aman Bisht <bishtaman60@gmail.com>
2024-08-16 18:14:27 +0530

    setup the project

---
+++ src/main/java/com/aman/chat_web_socket_learning/ChatWebSocketLearningApplication.java
@@ -0,0 +1,13 @@
+package com.aman.chat_web_socket_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatWebSocketLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatWebSocketLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aman/chat_web_socket_learning/ChatWebSocketLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aman.chat_web_socket_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatWebSocketLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
